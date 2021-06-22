2840a5618151722a2b01a8fb0c07154d7a3dca0d
ishikajain04 <85635648+ishikajain04@users.noreply.github.com>
2021-06-22 22:06:33 +0530

    only senders file is uploading

---
+++ sender.py
@@ -0,0 +1,7 @@
+import socket
+s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+recv_addr=("127.0.0.1",9999)
+user_data=input("enter your message: ")
+newdata=user_data.encode('ascii')
+s.sendto(newdata, recv_addr)
+print("your message has been sent ....")
